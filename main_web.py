3db7e299563025b795f2d82a41ac50d03c8eb231
kenmarqua-spec <kenmarqua@gmail.com>
2025-10-30 04:51:51 -0400

    Update main_web.py

--- main_web.py
+++ main_web.py
@@ -1,25 +1,10 @@
-from npc import *
 from flask import Flask, jsonify
-import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "🌍 Disordered Earth Web Build is Running!"
-
-@app.route('/npc_data')
-def npc_data():
-    data = {
-        "npc": {
-            "name": "Aela",
-            "age": 24,
-            "job": "Hunter",
-            "knowledge": ["Stone tools", "Firemaking"],
-            "family": ["Tharn", "Eri"]
-        }
-    }
-    return jsonify(data)
+def home():
+    return "🌍 Disordered Earth Web Build is running!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
